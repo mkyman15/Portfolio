085e4a327143003d96a06f2cacd5bc8fb9719a67
mkyman15 <quinn.delacruz@gmail.com>
2024-06-07 15:58:23 -0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,89 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+ 
+export function DialogDemo() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Edit Profile</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input
+              id="name"
+              defaultValue="Pedro Duarte"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input
+              id="username"
+              defaultValue="@peduarte"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default function Home() {
+  return (
+    <>
+    <section className="flex gap-5 justify-center items-center w-full h-full ">
+      <div>
+        <div>
+          ImagePLaceHolder
+        </div>
+        <div>
+        College
+        </div>
+      </div>
+
+      <h1 className="text-5xl font-bold">
+        QC
+      </h1>
+
+      <div>
+        <div>
+          ImagePLaceHolder
+        </div>
+        <div>
+        College
+        </div>
+      </div>
+
+    </section>
+    
+    
+    </>
+  );
+}
