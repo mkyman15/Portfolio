94c5d36cef6ee8563eb0df494e07372e66d665d5
mkyman15 <quinn.delacruz@gmail.com>
2024-06-21 16:29:23 -0700

    Mapping project data

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,36 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-function Showcase({ showcaseTitle, direction }: any) {
+import { Description } from "@radix-ui/react-dialog";
+import Link from "next/link";
+
+const projectData: any = [
+  {
+    title: "Duck Hunt Webapp",
+    description:
+      "This duckhunt is a project trying to replicate Duck Hunt by using replit. I used many languages such as JavaScript, Html, Canvas Api for images, css, and object orienting programing.",
+    imageSrc: "/DuckHunt.png",
+    linkUrl: "/",
+  },
+  {
+    title: "News Sentiment Analysis",
+    description:
+      "Using replit and mutiple existing libraries such as beautiful soup and textblob I developed a webscarper that performed sentiment analysis including bias checking. ",
+    imageSrc: "DuckHunt.png",
+    linkUrl: "/",
+  },
+  {
+    title: "Your Fortune",
+    description:
+      "More of a side project I tried practicing using files to store data for each user that interacted with the site. That way when given a fortune you only get one a day.",
+    imageSrc: "/DuckHunt.png",
+    linkUrl: "/",
+  },
+];
+
+const collegeData: any = [];
+
+function Showcase({ showcaseTitle, direction, showcaseData }: any) {
   return (
     <Sheet>
       <SheetTrigger className="cursor-pointer hover:opacity-50 transition-all duration-200">
@@ -22,27 +51,50 @@ function Showcase({ showcaseTitle, direction }: any) {
         <SheetHeader>
           <SheetTitle>{showcaseTitle}</SheetTitle>
           <SheetDescription>
-            <div>
-              <Image
-                src="/DuckHunt.png"
-                width="400"
-                height="400"
-                alt="person programming"
-              />
-            </div>{" "}
+            <section className="flex flex-col gap-10 justify-between content-between h-full">
+              {showcaseData.map((item) => {
+                return (
+                  <div className="flex gap-5">
+                    {/* Left section */}
+                    <div>
+                      <Link href="/">
+                        <Image
+                          src="/DuckHunt.png"
+                          width="400"
+                          height="400"
+                          alt="person programming"
+                        />
+                      </Link>
+
+                      <p className="text-xl font-bold">{item.title}</p>
+                    </div>
+
+                    {/* Right section */}
+                    <div className="w-[200px]">
+                      <p>{item.description}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </section>
           </SheetDescription>
         </SheetHeader>
       </SheetContent>
     </Sheet>
   );
 }
+
 export default function Home() {
   return (
     <>
       <section className="flex gap-5 justify-center items-center w-full h-full min-h-full mt-[32rem]">
         <div>
           <div>
-            <Showcase showcaseTitle="College" direction="left" />
+            <Showcase
+              showcaseTitle="College"
+              direction="left"
+              showcaseData={collegeData}
+            />
           </div>
           <div>College</div>
         </div>
@@ -58,7 +110,11 @@ export default function Home() {
 
         <div>
           <div>
-            <Showcase showcaseTitle="Projects" direction="right" />
+            <Showcase
+              showcaseTitle="Projects"
+              direction="right"
+              showcaseData={projectData}
+            />
           </div>
           <div>Projects</div>
         </div>
---
+++ instructions.md
@@ -0,0 +1,3 @@
+git add .
+git commit -m "some message here"
+git push
