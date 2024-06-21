ab0ca3c66c98a02266cad6dab1afa4e4e08a3e2b
mkyman15 <quinn.delacruz@gmail.com>
2024-06-21 15:36:02 -0700

    addition of picture and components

--- app/page.tsx
+++ app/page.tsx
@@ -1,90 +1,68 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button"
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
- 
- function DialogDemo() {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+function Showcase({ showcaseTitle, direction }: any) {
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you&apos;re done
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  )
+    <Sheet>
+      <SheetTrigger className="cursor-pointer hover:opacity-50 transition-all duration-200">
+        <Image
+          src="/ComputerProgrammer.jpg"
+          width="400"
+          height="400"
+          alt="person programming"
+        />
+      </SheetTrigger>
+      <SheetContent className="!max-w-[800px] w-[800px]" side={direction}>
+        <SheetHeader>
+          <SheetTitle>{showcaseTitle}</SheetTitle>
+          <SheetDescription>
+            <div>
+              <Image
+                src="/DuckHunt.png"
+                width="400"
+                height="400"
+                alt="person programming"
+              />
+            </div>{" "}
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
 }
-
 export default function Home() {
   return (
     <>
-    <DialogDemo/>
-    <section className="flex gap-5 justify-center items-center w-full h-full ">
-      <div>
+      <section className="flex gap-5 justify-center items-center w-full h-full min-h-full mt-[32rem]">
         <div>
-          ImagePLaceHolder
+          <div>
+            <Showcase showcaseTitle="College" direction="left" />
+          </div>
+          <div>College</div>
         </div>
-        <div>
-        College
-        </div>
-      </div>
 
-      <h1 className="text-5xl font-bold">
-        QC
-      </h1>
+        <h1 className="text-5xl font-bold">
+          <Image
+            src="/QCsignature.png"
+            width="400"
+            height="400"
+            alt="signature"
+          />
+        </h1>
 
-      <div>
         <div>
-          ImagePLaceHolder
+          <div>
+            <Showcase showcaseTitle="Projects" direction="right" />
+          </div>
+          <div>Projects</div>
         </div>
-        <div>
-        College
-        </div>
-      </div>
-
-    </section>
-    
-    
+      </section>
     </>
   );
 }
